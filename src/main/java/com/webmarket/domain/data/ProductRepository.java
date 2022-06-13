276007df981bff01405ef914e33a744ce00344aa
Jiminleeee <jiminab94@naver.com>
2022-06-13 11:23:32 +0900

    Product Repository 작성

--- src/main/java/com/webmarket/domain/data/ProductRepository.java
+++ src/main/java/com/webmarket/domain/data/ProductRepository.java
@@ -8,24 +8,37 @@ import com.webmarket.domain.model.Product;
 
 //다형성
 public class ProductRepository {
-	public static void main(String[] args) {
-		// 삽입 삭제가 빈번할 때 항상 동일 성능 
-		//but, ArrayList보다 검색이 느리고 메모리를 조금 더 많이 필요
-		List<Product> products = new LinkedList<>();	
+	private List<Product> products = new ArrayList<>();
+	
+	public ProductRepository() {
+		Product phone = new Product("P1234", "iPhone 6s", 800000);
+		phone.setDescription("4.7-inch, 1334x750 Retina HD display");
+		phone.setCategory("Smart Phone");
+		phone.setManufacturer("Apple");
+		phone.setUnitsInStock(1000);
+		phone.setCondition("New");
 		
-		//크기가 커질 수록 삽입, 삭제가 느려짐, 내부적으로 배열이라 성능이 빠름
-		List<Product> products2 = new ArrayList<>();	
+		Product notebook = new Product("P1235", "LG PC 그램", 1500000);
+		notebook.setDescription("!4.7-inch, 1334x750 Retina HD display");
+		notebook.setCategory("!Smart Phone");
+		notebook.setManufacturer("!Apple");
+		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refubished");
 		
-		Product[] products3 = new Product[3];
-		sort(Arrays.asList(products3));
+		Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+		tablet.setDescription("?4.7-inch, 1334x750 Retina HD display");
+		tablet.setCategory("?Smart Phone");
+		tablet.setManufacturer("?Apple");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("Old");
 		
-		sort(products2);
-		
+		products.add(phone);
+		products.add(notebook);
+		products.add(tablet);
 	}
 	
-	public static List<Product> sort(List<Product> products) {
-		//정렬...
-		return new LinkedList<>();
+	public List<Product> getAllProducts() {
+		return products;
 	}
 	
 }
